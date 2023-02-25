d36fb687157037c88d9fbb70dda1051f7254f7dc
Juan Covarrubias <me@juan.fi>
2023-02-25 11:58:54 +0200

    BackEnd: Update DrinksService
    Rewrite filtering logic

--- backend/src/services/drinks.ts
+++ backend/src/services/drinks.ts
@@ -4,35 +4,39 @@ import { v1 as uuid } from 'uuid';
 const drinks: Drink[] = [];
 
 /**
- * Returns all saved drinks
- * @param type Filter drinks by a given type
+ * Returns saved drinks. Optional filtering by type and name of drink.
+ * @param type Filter drinks by their type: coffee / tea. (optional)
+ * @param nameToFilterBy Filter drinks by their name. (optional)
  * @returns An array of Drinks
  */
-const getMany = (type: DrinkType|undefined = undefined): Drink[] => {
-  if (type) {
-    return drinks.filter(drink => drink.type === type);
-  }
-
-  return drinks;
+const getMany = (type: DrinkType|undefined = undefined, nameToFilterBy: string|undefined = undefined): Drink[] => {
+  return drinks
+    .filter(drink => filterByType(drink, type))
+    .filter(drink => filterByName(drink, nameToFilterBy));
 };
 
 /**
- * Returns all drinks filtered by a given name
- * @param type Filter drinks by a given type
- * @param name String that has to be included in name
- * @returns An array of Drinks
+ * Determines if a Drink is of a given type. Returns true if type 
+ * is undefined.
  */
-const getManyByName = (type: DrinkType|undefined = undefined, name: string): Drink[] => {
-  const searchString = name.trim().toLowerCase();
-
-  if (type) {
-    const filteredByType = drinks.filter(drink => drink.type === type);
-    return filteredByType.filter(drink => drink.name.toLocaleLowerCase().includes(searchString));
-  }
-
-  return drinks.filter(drink => drink.name.toLocaleLowerCase().includes(searchString));
+const filterByType = (drink: Drink, type: DrinkType|undefined): boolean => {
+  return type ?
+    drink.type === type :
+    true;
 };
 
+/**
+ * Determines if a Drink name contains part of given string. Returns true 
+ * if nameToFilterBy is undefined.
+ */
+const filterByName = (drink: Drink, nameToFilterBy: string|undefined): boolean => {
+  if (!nameToFilterBy) { 
+    return true; 
+  }
+
+  const searchString = nameToFilterBy.trim().toLowerCase();
+  return drink.name.toLowerCase().includes(searchString);
+};
 
 /**
  * Creates a new drink and returns new Drink object
@@ -53,6 +57,5 @@ const createDrink = (drinkToCreate: NewDrink): Drink => {
 
 export default {
   getMany,
-  getManyByName,
   createDrink
 };
