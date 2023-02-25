06f50c90d37b1568444ec0cf1fbd18a3711f4c2c
Juan Covarrubias <me@juan.fi>
2023-02-25 11:07:15 +0200

    BackEnd: Type validation
    Create validation.ts. Validates basic types and custom Drink types

---
+++ backend/src/utils/validation.ts
@@ -0,0 +1,66 @@
+import { DrinkType, NewDrink, Roast } from '../types/Drink';
+import ValidationError from '../errors/ValidationError';
+
+export const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+export const parseString = (text: unknown): string => {
+  if (!text || !isString(text)) {
+    throw new ValidationError('Incorrect type, not string');
+  }
+
+  return text;
+};
+
+export const isNumber = (num: unknown): num is number => {
+  return typeof num === 'number' || num instanceof Number;
+};
+
+export const parseNumber = (numberToParse: unknown): number => {
+  if (!numberToParse || !isNumber(numberToParse)) {
+    throw new ValidationError('Incorrect type, not a number');
+  }
+
+  return numberToParse;
+};
+
+const isDrinkType = (drinkType: unknown): drinkType is DrinkType => {
+  return isString(drinkType) && (drinkType === 'coffee' || drinkType === 'tea');
+};
+
+export const parseDrinkType = (drinkType: unknown): DrinkType => {
+  if (!isDrinkType(drinkType)) {
+    throw new ValidationError('Incorrect type, not a DrinkType');
+  }
+
+  return drinkType;
+};
+
+const isRoast = (roast: unknown): roast is Roast => {
+  return isNumber(roast) && roast > 0 && roast < 6;
+};
+
+export const parseRoast = (roast: unknown): Roast => {
+  if (!isRoast(roast)) {
+    throw new ValidationError('Incorrect type, not a Roast');
+  }
+
+  return roast;
+};
+
+const isNewDrink = (newDrink: unknown): newDrink is NewDrink => {
+  return newDrink !== null && typeof newDrink === 'object' &&
+  'type' in newDrink && isDrinkType(newDrink.type) &&
+  'name' in newDrink && isString(newDrink.name) &&
+  'price' in newDrink && isNumber(newDrink.price) &&
+  'roast' in newDrink && isRoast(newDrink.roast);
+};
+
+export const parseNewDrink = (newDrink: unknown): NewDrink => {
+  if (!newDrink || !isNewDrink(newDrink)) {
+    throw new ValidationError('Incorrect type, not a NewDrink');
+  }
+
+  return newDrink;
+};
