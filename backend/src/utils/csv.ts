2bc514f6a9aa34b748937142964b53fceb546e8c
Juan Covarrubias <me@juan.fi>
2023-02-27 10:36:22 +0200

    BackEnd: Create CsvHelper

---
+++ backend/src/utils/csv.ts
@@ -0,0 +1,62 @@
+import csvParser from 'csv-parse';
+import csvStringify from 'csv-stringify';
+import { createReadStream, createWriteStream, mkdir, openSync, closeSync } from 'fs';
+import path from 'node:path';
+
+/**
+ * Reads a given .csv file and returns contents as an array
+ * @param directory Directory from root of package
+ * @param filename Name of file to read
+ * @throws Read stream errors and csv-pare errors
+ * @returns An array containing all rows of .csv (including header row)
+ */
+const readCsvFile = async (directory: string, filename: string): Promise<unknown[]> => {
+  const records: unknown[] = [];
+  const parser = createReadStream(path.join(__dirname, '..', '..', directory, filename))
+    .pipe(csvParser.parse());
+
+  for await (const record of parser) {
+    records.push(record);
+  }
+
+  return records;
+};
+
+/**
+ * Writes given data to a .csv file
+ * @param directory Directory from root of package
+ * @param filename Name of file to write
+ * @param header Header row values for .csv file
+ * @param data Data to write
+ * @throws Mkdir errors, write stream errors and csv-stringify errors
+ */
+const writeCsvFile = (directory: string, filename: string, header: string[], data: unknown[]) => {
+  mkdir(path.join(__dirname, '..', '..', directory), { recursive: true }, (error) => {
+    if (error) {
+      throw error;
+    }
+  });
+
+  const writableStream = createWriteStream(path.join(__dirname, '..', '..', directory, filename));
+  const stringifier = csvStringify.stringify({ header: true, columns: header });
+  for (const row of data) {
+    stringifier.write(row);
+  }
+  stringifier.pipe(writableStream);
+};
+
+/**
+ * Overwrites a given file with a new empty file
+ * @param directory Directory from root of package
+ * @param filename Name of file to clear
+ * @throws Close sync and open sync errors
+ */
+const clearCsvFile = (directory: string, filename: string) => {
+  closeSync(openSync(path.join(__dirname, '..', '..', directory, filename), 'w'));
+};
+
+export default {
+  readCsvFile,
+  writeCsvFile,
+  clearCsvFile
+};
