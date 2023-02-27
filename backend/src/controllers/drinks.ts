e939e64fc68aa80c54b4857fea6c6a1d8db49d46
Juan Covarrubias <me@juan.fi>
2023-02-27 10:39:35 +0200

    BackEnd: Unknown type on drinks endpoint returns 400

--- backend/src/controllers/drinks.ts
+++ backend/src/controllers/drinks.ts
@@ -1,20 +1,22 @@
 import express, { NextFunction, Request, Response } from 'express';
 import DrinksService from '../services/drinks';
+import { DrinkType } from '../types/Drink';
 import { parseDrinkType, parseNewDrink } from '../utils/validation';
 
 const router = express.Router();
 
-router.get('/', (request: Request, response: Response) => {
+router.get('/', (request: Request, response: Response, next: NextFunction) => {
   const typeOfDrinkString = request.query.type?.toString();
   const searchQuery = request.query.search?.toString();
 
   try {
-    const typeOfDrink = parseDrinkType(typeOfDrinkString);
+    const typeOfDrink: DrinkType|undefined = typeOfDrinkString 
+      ? parseDrinkType(typeOfDrinkString) 
+      : undefined;
     const drinks = DrinksService.getMany(typeOfDrink, searchQuery);
     response.json({ data: drinks });
-  } catch {
-    const drinks = DrinksService.getMany(undefined, searchQuery);
-    response.json({ data: drinks });
+  } catch (error) {
+    next(error);
   }
 });
 
